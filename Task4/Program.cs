5dfb178c6b5261cac092beaad19938b145c9b771
Grig <chiefy@mail.ru>
2022-08-31 16:40:25 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,11 @@
+﻿//7. Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает последнюю цифру этого числа.
+//456 -> 6
+//782 -> 2
+//918 -> 8
+
+Console.Clear();
+Console.Write("Enter three digits number: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine(a % 10);
