9313945353e80e2416702e740010aba4066d2d92
Grig <chiefy@mail.ru>
2022-08-31 23:10:04 +0300

    Add Home Work Task 8

---
+++ HomeWork_Task8/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.Clear();
+
+Console.Write("Enter number: ");
+int a = int.Parse(Console.ReadLine());
+
+for (int n = 2; n <= a; n += 2)
+{
+    Console.Write($"{n}, ");
+}
+    
+
+
+
+
+
+
+
