67baa8407c6fc88193749a8e4db85d9ba4158ab1
Grig <chiefy@mail.ru>
2022-08-31 11:36:10 +0300

    Add task0

---
+++ Task0/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат.
+// (число умноженное само на себя)
+Console.Clear();
+
+Console.WriteLine("Enter the number: ");
+int number = int.Parse(Console.ReadLine());
+int sqr = number*number;
+Console.WriteLine($"Square of number {number} is {sqr}");
+
+int sqr1 = Convert.ToInt32(Math.Pow(number,2));
+Console.WriteLine($"Square of number {number} is {sqr1}");
