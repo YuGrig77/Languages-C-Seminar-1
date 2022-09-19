f72d74046905ef4b84dc9ea92ebc164b99024b9d
Grig <chiefy@mail.ru>
2022-09-19 15:07:55 +0200

    Arranged repository

--- Task1_V1/Program.cs
+++ Task1_V1/Program.cs
@@ -7,16 +7,16 @@
 
 Console.Clear();
 
-Console.WriteLine("Enter first number: ");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Enter second number: ");
-int b = int.Parse(Console.ReadLine());
+Console.Write("Entrer two numbers: ");
+string[] num = Console.ReadLine().Split(' ');
+int a = int.Parse(num[0]);
+int b = int.Parse(num[1]);
 
 if (a == b * b)
 {
-    Console.WriteLine($"Yes - {a} is e square of {b}");
+   Console.WriteLine($"Yes - {a} is e square of {b}");
 }
 else
 {
-    Console.WriteLine($"No - {a} is not square of {b}");
+   Console.WriteLine($"No - {a} is not square of {b}");
 }
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -16,19 +16,6 @@ if(day == 5) Console.Write("It is Friday");
 if(day == 6) Console.Write("It is Saturday");
 if(day == 7) Console.Write("It is Sunday");
 if(day > 7) Console.WriteLine("Come on, it's a week!");
+
 // or last line could be
 //else {Console.WriteLine("Come on, it's a week!")};
-
-//Через массив:
-//string[] days = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
-//Console.WriteLine("Введите номер дня недели (от 1 до 7)");
-//int num = int.Parse(Console.ReadLine());
-//
-//if (num <= 7 && num => 1)
-//{
-//    Console.WriteLine($"День недели - {day[num-1]}");
-//}
-//else
-//{
-//  Console.WriteLine("Введенное число не корректно");
-//}
